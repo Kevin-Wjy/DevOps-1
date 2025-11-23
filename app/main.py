8b02ce3eb6f79d36903a4c94ed755596a042c70d
Kevin Wijaya <kevin-wijaya@outlook.com>
2025-11-23 17:05:00 +0700

    fixing format issue from main.py (detected by lint test)

--- app/main.py
+++ app/main.py
@@ -1,20 +1,23 @@
-# app/main.py
 from fastapi import FastAPI, HTTPException
+
 
 app = FastAPI()
 
 # Simpel database in-memory
 items = {"1": "Laptop", "2": "Keyboard"}
 
+
 @app.get("/")
 def read_root():
     return {"message": "Hello, DevOps!"}
+
 
 @app.get("/items/{item_id}")
 def read_item(item_id: str):
     if item_id in items:
         return {"item_id": item_id, "name": items[item_id]}
     raise HTTPException(status_code=404, detail="Item not found")
+
 
 @app.post("/items/{item_id}")
 def create_item(item_id: str, name: str):
